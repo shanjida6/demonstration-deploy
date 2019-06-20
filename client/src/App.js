9f59d86a95e2a22bf67f56cc8d3d82166cbe0779
Shanjida6 <shanjida.a@gmail.com>
2019-06-20 16:43:34 -0400

    AJAX call for students from backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    axios.get("/students")
+      .then(res => res.data)
+      .then(students => console.log(students))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello world, check the console for students upon mount
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
