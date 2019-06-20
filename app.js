24e08b8e999f613588ac3935c3fadd5916ccc560
Shanjida6 <shanjida.a@gmail.com>
2019-06-20 17:09:33 -0400

    changes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 1234; /* NEW ADDITION */
 
 app.get("/students", (req, res, next) => {
-  res.json([{id: 1, name: "Axel"}, {id: 2, name: "Chris"}, {id: 3, name: "Michelle"}]);
+  res.status(200).json([{id: 1, name: "Axel"}, {id:2, name: "Chris"}, {id: 3, name: "Michelle"}]);
 })
 
-app.listen(1234, () => {
-  console.log("Listening on port 1234");
-});
+app.listen(PORT, () => { /* NEW ADDITION */
+  console.log(`Listening on port ${PORT}!!!`);
+})
